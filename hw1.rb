2eb4ea42d8bf90656c4715b847f5554c61c6e424
yliou <yliou@berkeley.edu>
2017-09-20 15:30:36 -0700

    did first 2 questions

--- hw1.rb
+++ hw1.rb
@@ -1,11 +1,12 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  return (a+b)*(a+b)
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  arr=a.sort
+  return arr.map! {|item| item +1}
 end
 
 def combine_name(first_name, last_name)
